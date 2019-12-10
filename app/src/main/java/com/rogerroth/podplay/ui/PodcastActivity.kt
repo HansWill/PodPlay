1f7ad92cdc4b4e1e8a96b1d770ef0f67cff3ede2
RORoth <RORoth@PC-HEM00008219>
2019-12-10 11:59:53 +0100

    added search functionality

--- app/src/main/java/com/rogerroth/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/rogerroth/podplay/ui/PodcastActivity.kt
@@ -1,8 +1,12 @@
 package com.rogerroth.podplay.ui
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import com.rogerroth.podplay.R
 import com.rogerroth.podplay.repository.ItunesRepo
 import com.rogerroth.podplay.service.ItunesService
@@ -20,5 +24,17 @@ class PodcastActivity : AppCompatActivity() {
 		itunesRepo.searchByTerm("Android Developer") {
 			Log.i(TAG, "Results = $it")
 		}
+	}
+
+	override fun onCreateOptionsMenu(menu: Menu): Boolean {
+		val inflater = menuInflater
+		inflater.inflate(R.menu.menu_search, menu)
+		val  searchMenuItem = menu.findItem(R.id.search_item)
+		val searchView = searchMenuItem?.actionView as SearchView
+		val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+		searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+		return true
+
 	}
 }
